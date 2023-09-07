435ab96ecda6222a75045c19993445559461e4cf
ValkyrieCode <134930608+ValkyrieCode@users.noreply.github.com>
2023-09-07 15:39:10 +0545

    jjj

---
+++ mero_kitab/mero_kitab/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mero_kitab project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mero_kitab.settings')
+
+application = get_wsgi_application()
--- system/urls.py
+++ system/urls.py
@@ -4,10 +4,9 @@ from . import views
 app_name='system'
 urlpatterns=[
     path('',views.homepage,name='home'),
-    
     path('order',views.orderpage,name='order'),
     path('menu',views.menupage,name='menu'),
-    path('about',views.aboutpage,name=''),
+    path('about',views.aboutpage,name='about'),
     path('receipt/', views.receipt_view, name='receipt'),
  
 ]
